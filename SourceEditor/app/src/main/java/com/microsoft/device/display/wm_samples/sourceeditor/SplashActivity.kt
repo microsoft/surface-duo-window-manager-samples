186f3746ce479e71913dce9803be23acb3e4b18d
Craig Dunn <craig.dunn@microsoft.com>
2021-10-21 21:26:54 -0700

    [sourceeditor] splash screen test

---
+++ SourceEditor/app/src/main/java/com/microsoft/device/display/wm_samples/sourceeditor/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.microsoft.device.display.wm_samples.sourceeditor
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        //Thread.sleep(2_000) // debugging
+        // move to the app straight away
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+}
