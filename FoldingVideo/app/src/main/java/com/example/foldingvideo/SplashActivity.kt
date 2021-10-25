8411450b3db2a02fe9f8eeb4a7255ff24c92d5b3
Craig Dunn <craig.dunn@microsoft.com>
2021-10-25 14:00:25 -0700

    [foldingvideo] splash screen

---
+++ FoldingVideo/app/src/main/java/com/example/foldingvideo/SplashActivity.kt
@@ -0,0 +1,20 @@
+/*
+ *
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License.
+ *
+ */
+package com.example.foldingvideo
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        // move to the app straight away
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+}
